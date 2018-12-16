200633b9792984969150e3daddfca4005faa55c5
boyd brefeld <boydbrefeld@hotmail.com>
2018-12-16 15:38:56 +0100

    updated code

--- api/artists.js
+++ api/artists.js
@@ -5,7 +5,11 @@ const artistsRouter = express.Router();
 const db = new sqlite3.Database(process.env.TEST_DATABASE || '../database.sqlite')
 
 artistsRouter.get('/', (req,res,next) => {
-  db.all(`SELECT * FROM Artist WHERE is_currently_employed = 1`, (err, artists) => {
+
+  /* callback of get */
+  db.all(`SELECT * FROM Artist WHERE is_currently_employed = 1`, function(err, artists) {
+
+    /* callback of db.all */
     if (err) {
       next(err);
     } else {
@@ -15,14 +19,46 @@ artistsRouter.get('/', (req,res,next) => {
   });
 });
 
+
+artistsRouter.post('/', (req,res,next) => {
+
+  /* callback of post */
+  if (!req.body.artist.name || !req.body.artist.dateOfBirth || !req.body.artist.biography) {
+    res.status(400).send();
+  } else {
+    const isCurrentlyEmployed = req.body.artist.isCurrentlyEmployed === 0 ? 0 : 1;
+    };
+    db.run(`INSERT INTO Artist (name, dateOfBirth, biography, is_currently_employed)
+    VALUES (${req.body.artist.name}, ${req.body.artist.dateOfBirth}, ${req.body.artist.biography}, ${isCurrentlyEmployed})`, function(err) {
+
+      /* callback of db.run */
+      if (err) {
+        next(err);
+      } else {
+        db.get(`SELECT * FROM Artist WHERE id = ${this.lastID}`, function(err, artist) {
+
+          /* callback of db.get */
+          if (err) {
+            next(err);
+          } else {
+            res.status(201).json({artist: artist});
+          };
+        });
+      };
+    });
+});
+
+
 artistsRouter.param('atristId', (req,res,next,artistId) => {
-  db.get(`SELECT * FROM Artist WHERE id = ${artistId}`, (err, artistById) => {
-    console.log('test');
+
+  /* callback of param */
+  db.get(`SELECT * FROM Artist WHERE id = ${artistId}`, function(err, artistById) {
+
+    /* callback of db.get */
     if (err) {
       next(err);
     } else if (artistById) {
       req.artist = artistById;
-      console.log(artistById);
       next();
     } else {
       res.status(404).send();
@@ -30,8 +66,53 @@ artistsRouter.param('atristId', (req,res,next,artistId) => {
   });
 });
 
+
 artistsRouter.get('/:artistId', (req,res,next) => {
+
+  /* callback of get */
   res.status(200).json({artist: req.artist});
+});
+
+
+artistsRouter.put('/:artistId', (req,res,next) => {
+
+  /* callback of put */
+  if (!req.body.name || !req.body.artist.dateOfBirth || !req.body.artist.biography) {
+    res.status(400).send();
+  } else {
+    db.run(`UPDATE Artist SET name = ${req.body.artist.name}, dateOfBirth = ${req.body.artist.dateOfBirth}, biography = ${req.body.artist.biography}, is_currently_employed = ${req.body.artist.is_currently_employed} WHERE id = ${req.params.artistId}`, function(err) {
+
+      /* callback of db.run */
+      if (err) {
+        next(err);
+      } else {
+        db.get(`SELECT * FROM Artist WHERE id = ${req.params.artistId}`, function(err, artist) {
+
+          /* callback of db.get */
+          if (err) {
+            next(err);
+          } else {
+            res.status(200).json({artist: artist});
+          };
+        });
+      };
+    });
+  };
+});
+
+
+artistsRouter.delete('/:artistId', (req,res,next) => {
+
+  /* callback of delete */
+  db.run(`UPDATE Artist SET is_currently_employed = 0 WHERE id = ${req.artist}`, function(err) {
+
+    /* callback of db.run */
+    if (err) {
+      next(err);
+    } else {
+      res.status(200).send();
+    };
+  });
 });
 
 module.exports = artistsRouter;
